7240861c27b0f57a0c85a2725ce80ca42e3e7293
Ursa1201 <andrei.ursachi97@gmail.com>
2026-02-28 19:33:07 +0200

    reparat logica cod, scapat de mem leak, si schimbat locatie din char in char*

--- lucruSaptamana1.c
+++ lucruSaptamana1.c
@@ -7,17 +7,40 @@ struct Biblioteca {
 	int id;
 	char* denumire;
 	int nrAngajati;
-	char locatie;
+	char* locatie;
 	float valoareMarfa;
 };
 
-struct Biblioteca initializare(int id, const char* denumire, int nrAngajati, char locatie, float valoareMarfa) {
+struct Biblioteca initializare(int id, const char* denumire, int nrAngajati, const char* locatie, float valoareMarfa) {
 	struct Biblioteca b;
 	b.id = id;
 	b.denumire = (char*)malloc((strlen(denumire) + 1) * sizeof(char));
-	strcpy_s(b.denumire, (strlen(denumire) + 1, denumire);
+	strcpy_s(b.denumire, strlen(denumire) + 1, denumire);
 	b.nrAngajati = nrAngajati;
-	b.locatie = locatie;
+	b.locatie = (char*)malloc((strlen(locatie)+1)*sizeof(char));
+	strcpy_s(b.locatie, strlen(locatie) + 1, locatie);
 	b.valoareMarfa = valoareMarfa;
 	return b;
+}
+
+//functie de afisare
+void afisareBiblioteca(struct Biblioteca b) {
+	printf("ID bibliotecii este %d, se numeste %s, are un total de %d angajati, se afla in %s, contine o valoare a cartilor de aprox %5.2f milioane lei."
+		, b.id, b.denumire, b.nrAngajati, b.locatie, b.valoareMarfa);
+}
+
+void dezalocareBiblioteca(struct Biblioteca* b) {
+	if (!b) {
+		return;
+	}
+	free(b->denumire);
+	free(b->locatie);
+	b->denumire = NULL;
+	b->locatie = NULL;
+}
+
+void main() {
+	struct Biblioteca bib1 = initializare(1, "Carturesti", 13, "Bacau", 13.5);
+	afisareBiblioteca(bib1);
+	dezalocareBiblioteca(&bib1);
 }
