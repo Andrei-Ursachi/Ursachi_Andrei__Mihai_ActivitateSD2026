db3d8e31e280831389f7e9ad22f7e3ce8eff82b0
Ursa1201 <andrei.ursachi97@gmail.com>
2026-04-10 14:10:19 +0300

    cod complet pt exercitiu cu hashtable2

--- lucruHashTable2.c
+++ lucruHashTable2.c
@@ -29,6 +29,7 @@ Bere initializare(const char* marca, int cantitate, float volumAcool) {
 	strcpy_s(bere.marca, strlen(marca) + 1, marca);
 	bere.cantitate = cantitate;
 	bere.volumAcool = volumAcool;
+	return bere;
 }
 
 void afisareBere(Bere bere) {
@@ -40,4 +41,77 @@ void afisareListaBere(Nod* cap) {
 		afisareBere(cap->info);
 		cap = cap->next;
 	}
+}
+
+void inserareLaSfarsit(Nod** cap, Bere bere) {
+	Nod* nou = (Nod*)malloc(sizeof(Nod));
+	nou->next = NULL;
+	nou->info = bere;
+	if (*cap == NULL) {
+		*cap = nou;
+	}
+	else {
+		Nod* aux = *cap;
+		while (aux->next = NULL) {
+			aux = aux->next;
+		}
+		aux->next = nou;
+	}
+}
+
+HashTable initiializareHash(int size) {
+	HashTable table;
+	table.dimensiune = size;
+	table.vector = (Nod**)malloc(sizeof(Nod*) * size);
+	for (int i = 0; i < size;i++) {
+		table.vector[i] = NULL;
+	}
+	return table;
+}
+
+void afisareTabelaHash(HashTable table) {
+	for (int i = 0;i < table.dimensiune; i++) {
+		printf("\nPozitia %d", i);
+		printf(": ");
+		afisareListaBere(table.vector[i]);
+	}
+}
+
+int hash(int dim, int cantitate) {
+	return cantitate % dim;
+}
+
+void inserareTabelaHash(HashTable table, Bere bere) {
+	if (table.dimensiune > 0) {
+		int poz = hash(table.dimensiune, bere.cantitate);
+		if (poz >= 0 && poz < table.dimensiune) {
+			inserareLaSfarsit(&(table.vector[poz]), bere);
+		}
+	}
+}
+
+void dezalocareLista(Nod** cap) {
+	while ((*cap) != NULL) {
+		free((*cap)->info.marca);
+		Nod* copie = *cap;
+		*cap = (*cap)->next;
+		free(copie);
+	}
+}
+
+void dezalocareHash(HashTable table) {
+	for (int i = 0; i < table.dimensiune;i++) {
+		dezalocareLista(&(table.vector[i]));
+	}
+	free(table.vector);
+}
+
+void main() {
+	HashTable table1 = initiializareHash(3);
+	inserareTabelaHash(table1, initializare("Ursus", 500, 5));
+	inserareTabelaHash(table1, initializare("Heineken", 330, 4));
+	inserareTabelaHash(table1, initializare("Ciucas", 1000, 7.2));
+
+	afisareTabelaHash(table1);
+	dezalocareHash(table1);
 }
